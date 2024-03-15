d96365d89f344f918435a127f128f2bef4132d55
dongs <don0421865@gmail.com>
2024-03-16 00:29:25 +0900

    백준 3문제 추가

---
+++ 연습장.java
@@ -0,0 +1,17 @@
+package 백준문제_브론즈;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class 연습장 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		ArrayList list = new ArrayList(8000);
+
+	}
+}
